08a5406db01ef137743f3a8287bd496feecd11a0
ttyle <ttyle@10.164.63.23>
2021-11-12 11:55:43 +0100

    Clases vacias de las ventanas que quedan

--- strava1.0/src/ventanas/mainParaVerVentanas.java
+++ strava1.0/src/ventanas/mainParaVerVentanas.java
@@ -5,9 +5,9 @@ public class mainParaVerVentanas {
 	public static void main(String[] args) {
 		//new VentanaIniciarSesion();
 		//new VentanaPrincipal();
-		//new VentanaPerfil();
+		new VentanaPerfil();
 		//new VentanaCrearReto();
-		new VentanaCrearEntrenamiento();
+		//new VentanaCrearEntrenamiento();
 
 	}
 
